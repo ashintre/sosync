f60de36051acfb357da023c10c9abf0a028e09dc
the77thsecret <the77thsecret@45849fe6-aefa-7c35-bc81-5978e8a86664>
2010-11-26 03:44:49 +0000

    Description: Basic frame to create Android application in

---
+++ trunk/SoS.yNc_Android/src/com/buzzters/sosync/activity/LaunchScreen.java
@@ -0,0 +1,13 @@
+package com.buzzters.sosync.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchScreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
